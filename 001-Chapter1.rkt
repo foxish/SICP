61fba7871b173b658a4031b0013830211d27fb90
DarkCthulhu <anirudh4444@gmail.com>
2014-03-26 19:33:58 +0530

    changed names, modified lang

---
+++ 001-Chapter1.rkt
@@ -0,0 +1,60 @@
+#lang planet neil/sicp
+
+;squares
+(define (square size) (* size size))
+(define (abs x)
+  (if (< x 0) 
+      (- x) 
+      x))
+(abs (- (square 10)))
+
+;comparison
+(define a 1)
+(define b 1)
+(= a b)
+
+
+;1.3 Define a procedure which takes 3 numbers as arguments and returns the sum of squares of two larger numbers
+(define (sum_sq_larger_2 x y z)
+  (define x1 (max x y z))
+  (define x2 (min (max x y) (max y z) (max x z)))
+  (+ (square x1) (square x2)))
+
+(sum_sq_larger_2 1 2 3)
+
+;1.4 a-plus-abs-b
+(display "a-plus-abs-b\n")
+(define (a-plus-abs-b a b)
+  ((if (> b 0)
+       + 
+       -)
+   a b))
+
+(a-plus-abs-b 10 -11)
+
+;1.5 proof of lazy evaluation(?)
+(display "Infinite Recursion\n")
+(define (p) (p))
+(define (func x y)
+  (if (= x 0) 
+      x 
+      y))
+
+;(func 0 (p))
+
+
+;newton method of square roots
+(display "Square Roots\n")
+(define (sqrt_newton x tolerance)
+  (sqrt_helper x (/ x 2.0) tolerance))
+
+(define (sqrt_helper num guess tolerance)
+  (define avg ( / (+ (/ num guess) guess) 2))
+    (if (> (abs (- num (* avg avg))) tolerance)
+      (sqrt_helper num avg tolerance)
+      guess))
+
+(sqrt_newton 100 0.0000001)
+
+
+
